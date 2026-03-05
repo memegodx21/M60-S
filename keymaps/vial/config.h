97f9a27ccfba362fcf8028b3d676a52d25a29425
memegodx21 <53485856+memegodx21@users.noreply.github.com>
2026-03-04 19:00:00 -0500

    Add files via upload

---
+++ keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x98, 0x19, 0x11, 0x88, 0x16, 0x28, 0x83, 0x37}
+
+/* CONSIDER ADDING AN UNLOCK COMBO. SEE DOCUMENTATION. */
